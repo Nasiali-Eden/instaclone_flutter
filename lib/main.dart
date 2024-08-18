c5f1826ed868cdf79b3ba13a85acf334bdfcdcd7
nasiali-eden <nasieden0@gmail.com>
2024-08-18 22:20:41 +0300

    title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter',
+      title: 'Instaclone',
       theme: ThemeData(
         appBarTheme: const AppBarTheme(
           elevation: 1,
